be290b6dbee17ab2f3ba1bee678fe5854ae65ec0
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-05-25 11:42:24 -0300

    Saving theme selected by user implemented

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:gofast/app/core/interfaces/shared_repository_interface.dart';
 import 'package:gofast/app/core/interfaces/theme_app_interfaces.dart';
+import 'package:gofast/app/core/repositories/shared_repository.dart';
 import 'package:gofast/app/core/theme/app_theme_dark.dart';
 import 'package:gofast/app/core/theme/app_theme_light.dart';
 import 'package:mobx/mobx.dart';
@@ -8,8 +11,24 @@ part 'app_controller.g.dart';
 class AppController = _AppControllerBase with _$AppController;
 
 abstract class _AppControllerBase with Store {
+  SharedRepository sharedRepository = Modular.get<ISharedRepositoryInterface>();
+
+  _AppControllerBase() {
+    getThemeData();
+  }
   @observable
   IThemeAppInterface themeApp = AppThemeLight();
+
+  @action
+  getThemeData() async {
+    await sharedRepository.getValue<String>('theme_mode').then(
+      (value) {
+        setThemeData(
+          value == 'ThemeMode.dark' ? ThemeMode.dark : ThemeMode.light,
+        );
+      },
+    );
+  }
 
   @action
   setThemeData(ThemeMode themeMode) {
--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,12 +2,17 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:gofast/app/app_controller.dart';
 import 'package:gofast/app/app_widget.dart';
+import 'package:gofast/app/core/interfaces/shared_repository_interface.dart';
+import 'package:gofast/app/core/repositories/shared_repository.dart';
 import 'package:gofast/app/modules/home/home_module.dart';
 import 'package:gofast/app/modules/splash/splash_page.dart';
 
 class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
+        Bind<ISharedRepositoryInterface>(
+          (i) => SharedRepository(),
+        ),
         Bind(
           (i) => AppController(),
         ),

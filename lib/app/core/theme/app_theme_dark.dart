2ddc3b7390a86499a172c8ba8915b4b8ba05c52c
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-05-25 10:17:38 -0300

    Released themes control Light/Dark

---
+++ lib/app/core/theme/app_theme_dark.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/src/material/theme_data.dart';
+import 'package:gofast/app/core/interfaces/theme_app_interfaces.dart';
+
+class AppThemeDark implements IThemeAppInterface {
+  @override
+  ThemeData getTheme() {
+    return ThemeData.dark();
+  }
+}
--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:gofast/app/app_controller.dart';
 import 'package:gofast/app/core/intercionalization/app_translate.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
         body: Center(
-          child: Text(AppTranslate(context).text('welcome')),
-        ));
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+            child: Text('Theme Dark'),
+            onPressed: () {
+              Modular.get<AppController>().setThemeData(ThemeMode.dark);
+            },
+          ),
+          RaisedButton(
+            child: Text('Theme Light'),
+            onPressed: () {
+              Modular.get<AppController>().setThemeData(ThemeMode.light);
+            },
+          ),
+        ],
+      ),
+    ));
   }
 }
